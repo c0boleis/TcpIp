4b491a73b911decf2b584fca55d5f2de09726a24
corentin <corentin@bata-linux>
2017-11-17 14:40:43 +0100

    création ihm

--- src/ihm/FrameConnection.java
+++ src/ihm/FrameConnection.java
@@ -1,17 +1,153 @@
 package ihm;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-public class FrameConnection extends JFrame {
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class FrameConnection extends JFrame{
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4891593274452416295L;
+	
+	private WindowAdapter windowAdapter;
+	
+	private PanelConnectionServer panelConnectionServer;
+	
+	private PanelConnectionClient panelConnectionClient;
+	
+	private JPanel panelConversation;
+	
+	private JTextArea textAreaConversation;
+	
+	private JTextArea textAreaSendMessage;
+	
+	private JButton buttonSend;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			InetAddress adr = InetAddress.getLocalHost();
+			System.out.println(adr.getHostAddress());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FrameConnection frame = new FrameConnection();
+		frame.setVisible(true);
 
+	}
+	
+	public FrameConnection() {
+		super();
+		this.setTitle("TcpIp");
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.addWindowListener(getWindowAdapter());
+		JPanel panelConnection = new JPanel();
+		panelConnection.setLayout(new BoxLayout(panelConnection, BoxLayout.Y_AXIS));
+		panelConnection.add(getPanelConnectionServer());
+		panelConnection.add(getPanelConnectionClient());
+		this.add(panelConnection, BorderLayout.CENTER);
+		this.add(getPanelConversation(),BorderLayout.SOUTH);
+	}
+
+	private PanelConnectionServer getPanelConnectionServer() {
+		if(panelConnectionServer == null) {
+			panelConnectionServer = new PanelConnectionServer();
+		}
+		return panelConnectionServer;
+	}
+
+	private PanelConnectionClient getPanelConnectionClient() {
+		if(panelConnectionClient == null) {
+			panelConnectionClient = new PanelConnectionClient();
+		}
+		return panelConnectionClient;
+	}
+	
+	private void close() {
+		//TODO
+		System.exit(0);
+	}
+
+	private WindowAdapter getWindowAdapter() {
+		if(windowAdapter == null) {
+			windowAdapter = new WindowAdapter() {
+
+				@Override
+				public void windowClosing(WindowEvent e) {
+					close();
+				}
+
+				@Override
+				public void windowClosed(WindowEvent e) {
+					close();
+				}
+				
+			};
+		}
+		return windowAdapter;
+	}
+
+	private JTextArea getTextAreaConversation() {
+		if(textAreaConversation == null) {
+			textAreaConversation = new JTextArea();
+		}
+		return textAreaConversation;
+	}
+
+	private JTextArea getTextAreaSendMessage() {
+		if(textAreaSendMessage == null) {
+			textAreaSendMessage = new JTextArea();
+		}
+		return textAreaSendMessage;
+	}
+
+	private JButton getButtonSend() {
+		if(buttonSend == null) {
+			buttonSend = new JButton();
+			buttonSend.setText("Send");
+			buttonSend.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+		return buttonSend;
+	}
+
+	public JPanel getPanelConversation() {
+		if(panelConversation == null) {
+			panelConversation = new JPanel();
+			panelConversation.setLayout(new BorderLayout());
+			JScrollPane paneConv = new JScrollPane(getTextAreaConversation());
+			paneConv.setPreferredSize(new Dimension(400, 150));
+			panelConversation.add(paneConv, BorderLayout.CENTER);
+			JScrollPane paneSend= new JScrollPane(getTextAreaSendMessage());
+			paneSend.setPreferredSize(new Dimension(400, 20));
+			panelConversation.add(paneSend, BorderLayout.SOUTH);
+			panelConversation.add(getButtonSend(), BorderLayout.EAST);
+		}
+		return panelConversation;
 	}
 
 }
---
+++ src/ihm/PanelConnectionServer.java
@@ -0,0 +1,71 @@
+package ihm;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class PanelConnectionServer extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7673958598150022247L;
+	
+	private JButton buttonConnection;
+	
+	private JLabel labelStatus;
+	
+	private JTextField textFieldIP;
+	
+	public PanelConnectionServer() {
+		this.setLayout(new BorderLayout());
+		this.add(getLabelStatus(), BorderLayout.WEST);
+		this.add(getTextFieldIP(), BorderLayout.CENTER);
+		this.add(getButtonConnection(), BorderLayout.EAST);
+		this.setPreferredSize(new Dimension(400, 20));
+		this.setBorder(new TitledBorder( "Server"));
+	}
+	
+	private JButton getButtonConnection() {
+		if(buttonConnection == null) {
+			buttonConnection = new JButton();
+			buttonConnection.setText("Connect");
+			buttonConnection.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					
+				}
+			});
+		}
+		return buttonConnection;
+	}
+
+	private JLabel getLabelStatus() {
+		if(labelStatus == null) {
+			labelStatus = new JLabel();
+			labelStatus.setOpaque(true);
+			labelStatus.setText("KO");
+			labelStatus.setBackground(Color.red);
+		}
+		return labelStatus;
+	}
+
+	private JTextField getTextFieldIP() {
+		if(textFieldIP == null) {
+			textFieldIP = new JTextField();
+		}
+		return textFieldIP;
+	}
+
+
+}

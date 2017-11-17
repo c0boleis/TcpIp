3a80f421d38716617e288d0e3248fda6b4785eb6
cb <corentinboleis@hotmail.fr>
2017-11-17 13:50:11 +0100

    init

---
+++ src/ihm/FrameConnection.java
@@ -0,0 +1,17 @@
+package ihm;
+
+import javax.swing.JFrame;
+
+public class FrameConnection extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4891593274452416295L;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
